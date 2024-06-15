b31a2c66535a5d784332f0c793060ee922641ce0
sandip <sandip28bit2021@kcc.edu.np>
2024-06-15 06:17:59 +0545

    :done:

--- sqlcommand.sql
+++ sqlcommand.sql
@@ -1,14 +1,3 @@
-CREATE TABLE spending_limit
-(
-    id          INT PRIMARY KEY AUTO_INCREMENT,
-    category_id int,
-    user_id     int,
-    amount      int,
-    date        DATE NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES expenses_category (category_id),
-    FOREIGN KEY (user_id) REFERENCES user_details (user_id)
-);
-
 CREATE TABLE user_details
 (
     user_id    INT PRIMARY KEY AUTO_INCREMENT,
@@ -19,17 +8,20 @@ CREATE TABLE user_details
 );
 
 
+
 CREATE TABLE income_category
 (
     category_id   INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(99) user_id int,
+    category_name VARCHAR(99), user_id int,
     FOREIGN KEY (user_id) REFERENCES user_details (user_id)
 );
 
 CREATE TABLE expenses_category
 (
     category_id   INT PRIMARY KEY AUTO_INCREMENT,
-    category_name VARCHAR(99) FOREIGN KEY(user_id) REFERENCES user_details(user_id)
+    category_name VARCHAR(99),
+    user_id int,
+     FOREIGN KEY(user_id) REFERENCES user_details(user_id)
 );
 
 CREATE TABLE income
@@ -56,10 +48,15 @@ CREATE TABLE expenses
     time              TIME(0),
     FOREIGN KEY (user_id) REFERENCES user_details (user_id),
     FOREIGN KEY (expenses_category) REFERENCES expenses_category (category_id)
-)
+);
 
-SELECT income_amount, category_name, remarks
-FROM income
-         INNER JOIN income_category ON income.income_category = income_category.category_id
-         INNER JOIN user_details ON income.user_id = user_details.user_id
-WHERE user_name = 'sandip';
+CREATE TABLE spending_limit
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    category_id int,
+    user_id     int,
+    amount      int,
+    date        DATE NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES expenses_category (category_id),
+    FOREIGN KEY (user_id) REFERENCES user_details (user_id)
+);
